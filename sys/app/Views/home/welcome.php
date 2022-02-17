86599b758d1a82f4c8a95c3fdeffaead871bc8ef
Edwin <mcedwin@unap.edu.pe>
2022-02-16 21:44:47 -0500

    actualizando interfaz

--- sys/app/Views/home/welcome.php
+++ sys/app/Views/home/welcome.php
@@ -96,70 +96,46 @@
                     <div class="p-2"><img src="https://i.imgur.com/Ur43esv.jpg" alt="user" width="50" class="rounded-circle"></div>
                     <div class="comment-text w-100">
                         <h6 class="font-medium">James Thomas</h6> <span class="m-b-15 d-block">This is awesome website. I would love to comeback again. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">April 14, 2019</span>  </div>
+                        <div class="comment-footer"> <span class="text-muted float-right">April 14, 2019</span> </div>
                     </div>
                 </div> <!-- Comment Row -->
                 <div class="d-flex flex-row comment-row">
                     <div class="p-2"><img src="https://i.imgur.com/8RKXAIV.jpg" alt="user" width="50" class="rounded-circle"></div>
                     <div class="comment-text active w-100">
                         <h6 class="font-medium">Michael Hussey</h6> <span class="m-b-15 d-block">Thanks bbbootstrap.com for providing such useful snippets. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">May 10, 2019</span>  </div>
+                        <div class="comment-footer"> <span class="text-muted float-right">May 10, 2019</span> </div>
                     </div>
                 </div> <!-- Comment Row -->
                 <div class="d-flex flex-row comment-row">
                     <div class="p-2"><img src="https://i.imgur.com/J6l19aF.jpg" alt="user" width="50" class="rounded-circle"></div>
                     <div class="comment-text w-100">
                         <h6 class="font-medium">Johnathan Doeting</h6> <span class="m-b-15 d-block">Great industry leaders are not the real heroes of stock market. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">August 1, 2019</span>  </div>
+                        <div class="comment-footer"> <span class="text-muted float-right">August 1, 2019</span> </div>
                     </div>
                 </div>
             </div> <!-- Card -->
         </div>
     </div>
     <div class="col-md-4">
-        <div class="card rounded">
+        <div class="card side-miembros mb-2">
+            <div class="card-header">Miembros</div>
             <div class="card-body">
-                <h6 class="card-title">latest photos</h6>
-                <div class="latest-photos">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""> </figure>
+                <div class="row">
+                    <?php for ($i = 0; $i < 18; $i++) : ?>
+                        <div class="col-2">
+                            <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar<?php echo ($i + 1) % 8 + 1; ?>.png" alt="">
                         </div>
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar4.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar5.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure class="mb-0"> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure class="mb-0"> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar8.png" alt=""> </figure>
-                        </div>
-                        <div class="col-md-4">
-                            <figure class="mb-0"> <img class="img-fluid" src="https://bootdey.com/img/Content/avatar/avatar9.png" alt=""> </figure>
-                        </div>
-                    </div>
+                    <?php endfor; ?>
                 </div>
             </div>
         </div>
 
-        <section class="card card-default">
+        <section class="card mb-2">
             <header class="card-header">
                 Comunicados
             </header>
             <div class="card-body">
-                <div class="row profile-activity">
+                <div class="row">
                     <div class="col-xs-10 col-sm-11">
                         <h6><a href="page-sidebar.html">Lorem ipsum dolor sit amet</a></h6>
                         <p> Lorem ipsum dolor sit amet.</p>
@@ -176,24 +152,83 @@
         </section>
 
         <section class="card card-default">
-            <header class="card-header">
-                Última encuesta
-            </header>
+
+            <img src="<?php echo base_url('sys/assets/img/poll.jpg'); ?>" class="card-img-top" alt="...">
             <div class="card-body">
-                <div class="row profile-activity">
-                    <div class="col-xs-10 col-sm-11">
-                        <h6><a href="page-sidebar.html">Lorem ipsum dolor sit amet</a></h6>
-                        <p> Lorem ipsum dolor sit amet.</p>
-                    </div>
-                    <div class="col-sm-12">
-                        <hr class="half-margins">
-                    </div>
-                    <div class="col-xs-10 col-sm-11">
-                        <h6><a href="page-sidebar.html">Sed diam nonummy nibh euismod</a></h6>
-                        <p> Lorem ipsum dolor sit amet.</p>
-                    </div>
+
+                ¿Cuál de los siguientes ciudadanos cree usted que podría ser el futuro gobernador de la región Puno?
+
+
+                <div class="poll-area mt-2">
+                    <input type="radio" name="poll" id="opt-1">
+                    <input type="radio" name="poll" id="opt-2">
+                    <input type="radio" name="poll" id="opt-3">
+                    <input type="radio" name="poll" id="opt-4">
+                    <label for="opt-1" class="opt-1">
+                        <div class="rowi">
+                            <div class="column">
+                                <span class="circle"></span>
+                                <span class="text">Alexander Flores Pari</span>
+                            </div>
+                            <span class="percent">30%</span>
+                        </div>
+                        <div class="progress" style='--w:30;'></div>
+                    </label>
+                    <label for="opt-2" class="opt-2">
+                        <div class="rowi">
+                            <div class="column">
+                                <span class="circle"></span>
+                                <span class="text">Richard Hancco Soncco</span>
+                            </div>
+                            <span class="percent">20%</span>
+                        </div>
+                        <div class="progress" style='--w:20;'></div>
+                    </label>
+                    <label for="opt-3" class="opt-3">
+                        <div class="rowi">
+                            <div class="column">
+                                <span class="circle"></span>
+                                <span class="text">Wilber Cutipa Alejo</span>
+                            </div>
+                            <span class="percent">40%</span>
+                        </div>
+                        <div class="progress" style='--w:40;'></div>
+                    </label>
+                    <label for="opt-4" class="opt-4">
+                        <div class="rowi">
+                            <div class="column">
+                                <span class="circle"></span>
+                                <span class="text">Hugo Quinto Huamán</span>
+                            </div>
+                            <span class="percent">10%</span>
+                        </div>
+                        <div class="progress" style='--w:10;'></div>
+                    </label>
+                </div>
+                <div class="text-end">
+                    <button type="submit" class="btn btn-primary">Votar</button>
                 </div>
             </div>
         </section>
     </div>
 </div>
+
+<script>
+    const options = document.querySelectorAll("label");
+    for (let i = 0; i < options.length; i++) {
+        options[i].addEventListener("click", () => {
+            for (let j = 0; j < options.length; j++) {
+                options[j].classList.remove("selected");
+            }
+
+            options[i].classList.add("selected");
+            for (let k = 0; k < options.length; k++) {
+                options[k].classList.add("selectall");
+            }
+
+            let forVal = options[i].getAttribute("for");
+            let selectInput = document.querySelector("#" + forVal);
+            let getAtt = selectInput.getAttribute("type");
+        });
+    }
+</script>
