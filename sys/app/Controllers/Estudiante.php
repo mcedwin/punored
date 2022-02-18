19ee879fe544b180f598d8a6a4fd7820660f110c
omms <ommskara@gmail.com>
2022-02-18 13:54:01 -0500

    corrected

--- sys/app/Controllers/Estudiante.php
+++ sys/app/Controllers/Estudiante.php
@@ -21,19 +21,8 @@ class Estudiante extends BaseController
         helper('formulario');
 		$datos['id'] = '0';
 		$datos['titulo'] = 'Registrar Estudiante';
-<<<<<<< HEAD
 		$datos['fields'] = $this->model->get();
-=======
-		$datos['fields'] = $model->get();
-        $this->addCss(array('lib/select2/dist/css/select2.min'));
-		$this->addJs(array(
-			"lib/tinymce/tinymce.min.js",
-			"lib/tinymce/jquery.tinymce.min.js",
-
-			'js/entrada/publicar.js'
-		));
-
->>>>>>> a23c4c15473089fd26a337b5610570c0091389f5
+    
 		$this->showHeader();
 		$this->ShowContent('form', $datos);
 		$this->showFooter();
