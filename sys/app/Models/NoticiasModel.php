a9d2a245b2cd124f3630aac935c633036601c12b
omms <ommskara@gmail.com>
2022-02-21 10:54:53 -0500

    listado noticias sin paginacion

--- sys/app/Models/NoticiasModel.php
+++ sys/app/Models/NoticiasModel.php
@@ -6,9 +6,19 @@ use CodeIgniter\Model;
 
 class NoticiasModel extends Model
 {
-    protected $table = 'tableName';
+    protected $table = 'entrada';
 
     public function getDataListaNoticia(){
-
+      $builder = $this->db->table($this->table);
+      $result = $builder->select([
+      'entr_descripcion',
+      'entr_foto',
+      'entr_url',
+      'entr_fechapub'
+      ])
+        ->select('usua_nombres')
+        ->join('usuario','usua_id = entr_usua_id','left')
+      ->get()->getResultArray();
+      return $result;
     }
 }
