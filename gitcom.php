90447b2bb5aa43bd58b4d1fc7a20c79cec531e19
Edwin <mcedwin@gmail.com>
2023-05-03 21:34:29 -0500

    actualizando

--- gitcom.php
+++ gitcom.php
@@ -1,8 +1,12 @@
-<?php
-$path = "/home4/dofondo/punored.com/"; 
-$a='';
-chdir($path);
-//exec("git add .");  
-exec("git pull");
-echo "<h3 align = center> Succesfully commited all the files.</h3>";
+<?php
+
+$path = "/home4/dofondo/punored.com/"; 
+
+$a='';
+chdir($path);
+//exec("git add .");  
+exec("git pull",$output, $return_var);
+var_dump($output);
+echo "<h3 align = center> Succesfully commited all the files.</h3>";
+
 ?>
